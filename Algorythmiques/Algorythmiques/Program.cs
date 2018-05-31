987cfc49e9a7d72315b67e164ee77e0e01a027c6
jessy974 <jessy@laniel.me>
2018-05-31 16:58:44 +0200

    Version2

--- Algorythmiques/Algorythmiques/Program.cs
+++ Algorythmiques/Algorythmiques/Program.cs
@@ -23,5 +23,33 @@ namespace Algorythmiques
             choix = int.Parse(Console.ReadLine());
             return choix;
         }
+
     }
+    class MenuItem
+    {
+        public int Numero { get; set; }
+        public string Libelle { get; set; }
+
+        private static int VerifierMenu(List<MenuItem> menu)
+        {
+            foreach (MenuItem ligne in menu)
+
+            {
+                Console.WriteLine($"{ligne.Numero}-{ligne.Libelle}");
+            }
+            int Choix;
+            Choix = int.Parse(Console.ReadLine());
+
+            foreach (MenuItem numero in menu)
+            {
+                if (Choix == numero.Numero)
+                {
+                    return Choix;
+                }
+            }
+            Console.WriteLine("Erreur lors de la saisie");
+            return -1;
+        }
+    }
+
 }
